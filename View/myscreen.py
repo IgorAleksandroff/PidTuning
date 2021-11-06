d00b7d587b53778dc62113f2d981e83f5942d596
Igor-Aleksandrov <igoruale@gmail.com>
2021-11-06 21:54:53 +0500

    added pid table on Tab2

--- View/myscreen.py
+++ View/myscreen.py
@@ -27,7 +27,7 @@ class MyScreenView(MDScreen, Observer):
     # <Model.myscreen.MyScreenModel object>.
     model = ObjectProperty()
 
-    data_tables = None
+    pi_tables, pid_tables = None, None
 
     def __init__(self, **kw):
         super().__init__(**kw)
@@ -58,29 +58,54 @@ class MyScreenView(MDScreen, Observer):
         self.ids.i.text = str(round(self.model.i, 2))
         self.ids.d.text = str(round(self.model.d, 2))
 
-        row_data_for_tab = [(
-            "1",
-            "Cohen-Coon",
-            str(round(self.model.p, 2)),
-            str(round(self.model.i, 2)),
-            str(round(self.model.d, 2)),
+        row_pi_for_tab = [
+            (
+                "1",
+                "Cohen-Coon",
+                str(round(self.model.pp, 2)),
+                str(round(self.model.ii, 2)),
+                "-",
+            ),
+        ]
+        row_pid_for_tab = [
+            (
+                "1",
+                "Cohen-Coon",
+                str(round(self.model.p, 2)),
+                str(round(self.model.i, 2)),
+                str(round(self.model.d, 2)),
+                "-",
             ),
         ]
 
-        self.data_tables = MDDataTable(
+        self.pi_tables = MDDataTable(
             size_hint = (0.9, 0.9),
             # name column, width column, sorting function column(optional)
             column_data = [
                 ("No.", dp(10)),
                 ("Method", dp(30)),
-                ("P", dp(10)),
-                ("I", dp(10)),
-                ("D", dp(10)),
+                ("P", dp(15)),
+                ("I", dp(15)),
             ],
-            row_data = row_data_for_tab
+            row_data = row_pi_for_tab,
         )
-        self.ids.calc_data_table.clear_widgets()
-        self.ids.calc_data_table.add_widget(self.data_tables)
+        self.ids.calc_pi_table.clear_widgets()
+        self.ids.calc_pi_table.add_widget(self.pi_tables)
+
+        self.pid_tables = MDDataTable(
+            size_hint = (0.9, 0.9),
+            # name column, width column, sorting function column(optional)
+            column_data = [
+                ("No.", dp(10)),
+                ("Method", dp(30)),
+                ("P", dp(15)),
+                ("I", dp(15)),
+                ("D", dp(15)),
+            ],
+            row_data = row_pid_for_tab,
+        )
+        self.ids.calc_pid_table.clear_widgets()
+        self.ids.calc_pid_table.add_widget(self.pid_tables)
 
     def on_tab_switch(self, *args):
         self.current_tab = args[1].name
